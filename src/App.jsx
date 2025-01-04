2ad3a9f17fe2c249c406449b0505de378e30e453
riddsep <ridoseptiawan00@gmail.com>
2025-01-04 11:20:18 +0700

    init project

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,5 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Heading from "./ui/Heading";
-import Input from "./ui/Input";
-import Row from "./ui/Row";
-
 function App() {
-  return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">The Wild Oasis</Heading>
-            <div>
-              <Heading as="h2">Check in and Check Out</Heading>
-              <Button size="medium" variation="primary">
-                Check in
-              </Button>
-              <Button size="small" variation="secondary">
-                Check out
-              </Button>
-            </div>
-          </Row>
-          <Row type="vertical">
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input type="number" placeholder="Number of Guests" />
-              <Input type="number" placeholder="Number of Guests" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
-  );
+  return <div></div>;
 }
-
-const StyledApp = styled.main`
-  padding: 20px;
-`;
 
 export default App;

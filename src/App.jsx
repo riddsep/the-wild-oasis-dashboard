1d61179c405568ba5d927536385aa7d8e251df70
riddsep <ridoseptiawan00@gmail.com>
2025-01-04 07:56:30 +0700

    init project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
 function App() {
-  return <div>Heloo</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>Heloo</H1>
+      </div>
+    </>
+  );
 }
+
+const H1 = styled.h1`
+  font-size: 50px;
+  color: var(--color-brand-900);
+`;
 
 export default App;

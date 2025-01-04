1f55a00c577cf8e8360b71fc636f093040a0fbe2
riddsep <ridoseptiawan00@gmail.com>
2025-01-04 10:36:18 +0700

    init project

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,41 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Heading from "./ui/Heading";
 import Input from "./ui/Input";
+import Row from "./ui/Row";
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Heloo</Heading>
-        <Heading as="h2">Heloo</Heading>
-        <Button>Click me</Button>
-        <Button>Click me</Button>
-        <Heading as="h3">Heloo</Heading>
-        <Input type="number" />
-        <Input type="number" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and Check Out</Heading>
+              <Button size="medium" variation="primary">
+                Check in
+              </Button>
+              <Button size="small" variation="secondary">
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of Guests" />
+              <Input type="number" placeholder="Number of Guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
 }
 
 const StyledApp = styled.main`
-  background-color: lightblue;
-  width: 100%;
-  max-width: 500px;
-  margin-inline: auto;
+  padding: 20px;
 `;
 
 export default App;

914a5a9ea25a5d61eb1d12fd920176f65bceb82d
riddsep <ridoseptiawan00@gmail.com>
2025-01-05 21:12:48 +0700

    minor changes

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -9,3 +9,12 @@ export async function getCabins() {
 
   return cabins;
 }
+
+export async function deleteCabin(id) {
+  const { error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
+  }
+}

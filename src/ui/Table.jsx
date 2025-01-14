df90bb32d72d8f120aed5a0758f6a182f10978ec
riddsep <ridoseptiawan00@gmail.com>
2025-01-14 14:15:25 +0700

    feat: cabin section

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -87,7 +87,7 @@ function Row({ children }) {
   );
 }
 function Body({ data, render }) {
-  if (!data?.length) return <Empty>No data to show</Empty>;
+  if (!data?.length) return <Empty>data</Empty>;
 
   return <StyledBody>{data.map(render)}</StyledBody>;
 }

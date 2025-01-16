0973326e1f5e486a095646ac00413f40a6b232c6
riddsep <ridoseptiawan00@gmail.com>
2025-01-16 21:34:56 +0700

    minor changes

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,43 +1,43 @@
-import Spinner from "../../ui/Spinner";
-import CabinRow from "./CabinRow";
-import { useCabin } from "./useCabin";
-import Table from "../../ui/Table";
-import Menus from "../../ui/Menus";
-import { useSearchParams } from "react-router-dom";
+// import Spinner from "../../ui/Spinner";
+// import CabinRow from "./CabinRow";
+// import { useCabin } from "./useCabin";
+// import Table from "../../ui/Table";
+// import Menus from "../../ui/Menus";
+// import { useSearchParams } from "react-router-dom";
 
-function CabinTable() {
-  const [searchParams] = useSearchParams();
-  const { isLoading, cabins } = useCabin();
+// function CabinTable() {
+//   const [searchParams] = useSearchParams();
+//   const { isLoading, cabins } = useCabin();
 
-  if (isLoading) return <Spinner />;
+//   if (isLoading) return <Spinner />;
 
-  const filterValue = searchParams.get("discount") || "all";
+//   const filterValue = searchParams.get("discount") || "all";
 
-  let filteredCabins;
-  if (filterValue === "all") filteredCabins = cabins;
-  if (filterValue === "no-discount")
-    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
-  if (filterValue === "with-discount")
-    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+//   let filteredCabins;
+//   if (filterValue === "all") filteredCabins = cabins;
+//   if (filterValue === "no-discount")
+//     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+//   if (filterValue === "with-discount")
+//     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
 
-  return (
-    <Menus>
-      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-        <Table.Header>
-          <div></div>
-          <div>Cabin</div>
-          <div>Capacity</div>
-          <div>Price</div>
-          <div>Discount</div>
-          <div></div>
-        </Table.Header>
-        <Table.Body
-          data={filteredCabins}
-          render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
-        />
-      </Table>
-    </Menus>
-  );
-}
+//   return (
+//     <Menus>
+//       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+//         <Table.Header>
+//           <div></div>
+//           <div>Cabin</div>
+//           <div>Capacity</div>
+//           <div>Price</div>
+//           <div>Discount</div>
+//           <div></div>
+//         </Table.Header>
+//         <Table.Body
+//           data={filteredCabins}
+//           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+//         />
+//       </Table>
+//     </Menus>
+//   );
+// }
 
-export default CabinTable;
+// export default CabinTable;

242c992f0b6fdcd5c812166a7f515d1cd26cf782
riddsep <ridoseptiawan00@gmail.com>
2025-01-17 09:37:44 +0700

    feat: filter table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,12 +4,14 @@ import { useCabin } from "./useCabin";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const [searchParams] = useSearchParams();
   const { isLoading, cabins } = useCabin();
 
   if (isLoading) return <Spinner />;
+  if (!cabins.length) return <Empty resourceName="cabins" />;
 
   const filterValue = searchParams.get("discount") || "all";
 
